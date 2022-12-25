8650f317c5c242b951f0363ea2fac00bed72b1f0
DESKTOP-BPSL7M8\admin <dhk21512@gmail.com>
2022-12-25 22:03:22 +0900

    cors 설정

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const http = require("http");
+
+dotenv.config();
 
 const port = 3001;
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors());
+app.options("*", cors());
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+
+app.use();
 
 app.use("/", (req, res) => {
   res.json("hello world");
